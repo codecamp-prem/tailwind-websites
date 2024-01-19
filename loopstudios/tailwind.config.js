388a3e454de5af43b58eff03682b348f36975ed1
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-19 13:38:45 +0545

    setup & config

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screen: {
+      sm: "480px",
+      md: "769px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+    },
   },
   plugins: [],
-}
+};
