98d8ac422cd01213fdbdaea7570286a2102c4cec
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-23 20:11:29 +0545

    mobile menu & its js functionality

--- bookmarkManager/js/index.js
+++ bookmarkManager/js/index.js
@@ -1,3 +1,4 @@
+/* Tabs */
 const tabs = document.querySelectorAll(".customClassTab");
 const panels = document.querySelectorAll(".panel");
 
@@ -22,3 +23,27 @@ function onTabClick(e) {
     .getElementsByClassName(e.target.getAttribute("data-target"))[0]
     .classList.remove("hidden");
 }
+
+// hambuger
+const hamBtn = document.getElementById("menu-btn");
+const hamMenu = document.getElementById("menu");
+const changeTopLogo = document.getElementById("logo");
+
+hamBtn.addEventListener("click", navToggle);
+
+function navToggle() {
+  hamBtn.classList.toggle("open");
+  hamMenu.classList.toggle("flex");
+  hamMenu.classList.toggle("hidden");
+
+  if (hamMenu.classList.contains("flex")) {
+    changeTopLogo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    changeTopLogo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
+
+// close hambuger on one of the mobile menu list clicked
+document.querySelectorAll(".mobileMenu").forEach((item) => {
+  item.addEventListener("click", navToggle);
+});
