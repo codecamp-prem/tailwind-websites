6925b5dc57688d9675b9dc825a76abf83d44e35c
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-26 14:59:53 +0545

    show testimonials section gallery images in Custom lightbox

--- caferio/js/index.js
+++ caferio/js/index.js
@@ -21,3 +21,31 @@ function closeMenu() {
 document.querySelectorAll(".nav-link").forEach((item) => {
   item.addEventListener("click", closeMenu);
 });
+
+/* light box */
+const lightBox = document.querySelector(".lightbox");
+const lightBoxBody = document.querySelector(".lightbox-body");
+
+const images = document.querySelectorAll(".testimonials-gallery-img");
+
+images.forEach(function (image) {
+  image.addEventListener("click", function () {
+    lightBox.classList.remove("scale-0");
+    lightBox.classList.add("scale-100");
+    const img = document.createElement("img");
+    img.src = image.src;
+    img.classList.add("w-full");
+    img.classList.add("h-full");
+    img.classList.add("image-cover");
+    if (lightBoxBody.children[0]) {
+      lightBoxBody.removeChild(lightBoxBody.children[0]);
+    }
+    lightBoxBody.appendChild(img);
+  });
+});
+
+lightBox.addEventListener("click", function (e) {
+  if (!e.target.getAttribute("id")) return;
+  lightBox.classList.add("scale-0");
+  lightBox.classList.remove("scale-100");
+});
